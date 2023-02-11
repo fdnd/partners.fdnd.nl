9e889ae2ba60ec51e39f28370bc57bf1801e691a
joostf <j.faber@hva.nl>
2023-02-11 17:25:04 +0100

    tweaks

--- src/routes/[project]/+page.svelte
+++ src/routes/[project]/+page.svelte
@@ -9,57 +9,58 @@
 		return groupOfPeople.length > 1 ? suffix : '';
 	}
 </script>
+<div class="wrapper">
+	<section id="information">
+		<h2>{data.project.title}</h2>
 
-<section id="information">
-	<h2>{data.project.title}</h2>
+		<section class="rounded green-on-blue">
+			<h3>Teamleden</h3>
+			<ul>
+				<li><span>Opdrachtgever</span> <a href="/">{data.project.clients[0].title}</a></li>
+				<li>
+					<span>Product owner{isPlural(data.project.productOwners, 's')}</span>  
+					{#each data.project.productOwners as productOwner, i}
+						<a href="/" target="_blank" rel="noopener noreferrer">{getFullName(productOwner)}</a>
+					{/each}
+				</li>
 
-	<section class="rounded green-on-blue">
-		<h3>Teamleden</h3>
-		<ul>
-			<li><span>Opdrachtgever</span> <a href="/">{data.project.clients[0].title}</a></li>
-			<li>
-				<span>Product owner{isPlural(data.project.productOwners, 's')}</span>  
-				{#each data.project.productOwners as productOwner, i}
-					<a href="/" target="_blank" rel="noopener noreferrer">{getFullName(productOwner)}</a>
+				<li>
+					<span>Coach{isPlural(data.project.coaches, 'es')}</span>
+					{#each data.project.coaches as coach, i}
+						<a href="/" target="_blank" rel="noopener noreferrer">{getFullName(coach)}</a>
+					{/each}
+				</li>
+
+				{#if data.project.teamLeads.length > 0 }
+				<li>
+					<span>Team lead{isPlural(data.project.teamLeads, 's')}</span>
+					{#each data.project.teamLeads as teamlead}
+						<a href="/" target="_blank" rel="noopener noreferrer">{getFullName(teamlead)}</a>
+					{/each}
+				</li>
+				{/if}		
+			</ul>
+
+
+			<h4>Studenten</h4>
+			<ul>
+				{#each data.project.students as student}
+					<li><a href="/" target="_blank" rel="noopener noreferrer">{getFullName(student)}</a></li>
 				{/each}
-			</li>
+			</ul>
+		</section>
 
-			<li>
-				<span>Coach{isPlural(data.project.coaches, 'es')}</span>
-				{#each data.project.coaches as coach, i}
-					<a href="/" target="_blank" rel="noopener noreferrer">{getFullName(coach)}</a>
-				{/each}
-			</li>
-
-			{#if data.project.teamLeads.length > 0 }
-			<li>
-				<span>Team lead{isPlural(data.project.teamLeads, 's')}</span>
-				{#each data.project.teamLeads as teamlead}
-					<a href="/" target="_blank" rel="noopener noreferrer">{getFullName(teamlead)}</a>
-				{/each}
-			</li>
-			{/if}		
-		</ul>
-
-
-		<h4>Studenten</h4>
-		<ul>
-			{#each data.project.students as student}
-				<li><a href="/" target="_blank" rel="noopener noreferrer">{getFullName(student)}</a></li>
-			{/each}
-		</ul>
+		<section class="rounded green-on-blue">
+			<h3 class="visually-hidden">Omschrijving</h3>
+			{#if data.project.visual.url}
+				<img src="{data.project.visual.url}" alt="{data.project.clients[0].title}" width="250">
+			{/if}
+			<div class="body">
+				{@html data.project.description.html}
+			</div>
+		</section>
 	</section>
-
-	<section class="rounded green-on-blue">
-		<h3 class="visually-hidden">Omschrijving</h3>
-		{#if data.project.visual.url}
-			<img src="{data.project.visual.url}" alt="{data.project.clients[0].title}" width="250">
-		{/if}
-		<div class="body">
-			{@html data.project.description.html}
-		</div>
-	</section>
-</section>
+</div>
 
 <section id="work">
 
@@ -81,6 +82,9 @@
 
 
 <style>
+	.wrapper {
+		/margin:0 -1rem;
+	}
 	section {
 		position: relative;
 		align-items: end;
@@ -156,6 +160,9 @@
 	}
 
 	@media (min-width: 50em) {
+		.wrapper {
+			margin:0;
+		}
 		img {
 			right:-2rem;
 			top:-2rem;
