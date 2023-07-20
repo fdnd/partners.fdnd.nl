939a48c635ff64e37f83b11fd99aba9b8016f046
joostf <j.faber@hva.nl>
2023-07-20 09:56:38 +0200

    added content

--- src/routes/samenwerken/+page.svelte
+++ src/routes/samenwerken/+page.svelte
@@ -3,14 +3,27 @@
 </script>
 
 
-<section class="rounded blue-on-green">
+<header class="rounded blue-on-green">
 	<h2>FDND Agency</h2>
 
-    <p>FDND is een 2-jarige, praktijgerichte HBO opleidingen, waarin aan projecten werken voor echte opdrachtgevers is ingebed in het onderwijs.</p>
-    <a href="https://fdnd.nl/">Bekijk meer informatie over FDND</a>
-</section>
+    <p>De projecten waar we bij FDND aan werken noemen we FDND Agency. <a href="https://fdnd.nl/">FDND</a> is een 2-jarige, praktijgerichte HBO opleidingen, waarin aan projecten werken voor echte opdrachtgevers is ingebed in het onderwijs.</p>
+    
+
+    <h4>Op deze pagina:</h4>
+    <ul>
+        <li><a href="#duurzame-samenwerking">Duurzame samenwerking</a></li>
+        <li><a href="#semesters-en-sprints">Semesters en sprints</a></li>
+        <li><a href="#backlog">Backlog</a></li>
+        <li><a href="#sprint-reviews">Sprint reviews</a></li>
+    </ul>
+</header>
 
 <section class="rounded green-on-blue">
+    <h3 class="visually-hidden">Impressie</h3>
+    <img src="/img/review-sprint-12.jpeg" alt="Review sprint 12" class="rounded">
+</section>
+
+<section class="rounded green-on-blue" id="duurzame-samenwerking">
     <h3>Duurzame samenwerking</h3>
     <p>FDND studenten werken aan kleine opdrachten (van een sprint) en aan grote projecten (van 3 semesters). De inzet is altijd een duurzame samenwerking.</p>
     <p>Projecten krijgen steeds meer betekenis door samen te kijken waar de mogelijkheden voor voor verdere iteraties liggen, bijvoorbeeld bij andere programma's of opleidingen binnen de HvA.</p>
@@ -18,20 +31,20 @@
     <p>In een langere samenwerking op meerdere vlakken ontstaat een betekenisvolle, duurzame samenwerking.</p>
 </section>
 
-<section class="rounded blue-on-green">
-	<h3>Sprints</h3>
+<section class="rounded blue-on-green" id="semesters-en-sprints">
+	<h3>Semesters & Sprints</h3>
     <p>Elk semester bestaat uit 6 sprints van ieder 3 weken. Tijdens de middelste 4 sprints itereren studententeams aan de hand van een backlog op de projecten.</p>
     <p>In totaal werken de studententeams, 12 sprints van ieder 3 weken aan de projecten. Er zit een opbouw in de semesters in complexiteit van de te bouwen web applicatie; In het <a href="https://programma.fdnd.nl/static-web">eerste semester</a> starten de studenten met het ontwerpen en bouwen van statische websites. In het <a href="https://programma.fdnd.nl/data-driven-web">tweede semester</a> is het uitgangspunt van de website dynamische data uit een API. Tenslotte in het <a href="https://programma.fdnd.nl/workflow-tooling-frameworks">derde semester</a> wordt het project afgerond naar een robuste gebruiksvriendelijke toepassing.</p>
     <a href="https://programma.fdnd.nl/">Bekijk hier het programma: programma.fdnd.nl</a>
 </section>
 
-<section class="rounded blue-on-green">
+<section class="rounded green-on-blue" id="backlog">
 	<h3>Backlog</h3>
     <p>De voortgang van het project wordt bijgehouden in een backlog. Voor elke sprints worden user stories klaargezet in de backlog. Aan de hand van de user stories werken de studenten aan d eprojecten. Omdat we de voortgang van projecten op deze manieren bijhouden kunnen we heel gericht projecten laten aansluiten bij andere programma's van andere opleidingen bij de HvA. </p>
     <img src="/img/backlog.png" alt="Voorbeeld van een backlog bij FDND">
 </section>
 
-<section class="rounded green-on-blue">
+<section class="rounded blue-on-green" id="sprint-reviews">
 	<h3>Sprint reviews</h3>
     <p>Elke sprint presenteren de studenten hun werk in een sprint review. Tijdens de review wordt de backlog bekeken en waar nodig aangepast, zodat het duidelijk is waar de volgende sprint aan gewerkt kan worden.</p>
 
@@ -41,10 +54,15 @@
 
 
 
+
+
 <style>
 	header {
-		margin-bottom: 2rem;
-		padding:1rem 2rem;
+		padding:2rem;
+        grid-area: auto;
+        
+        
+        max-width:40rem;
 	}
     section {
 		min-height:auto;
@@ -52,8 +70,5 @@
 		max-width:40rem;
 	}
 
-    section:first-child {
-        grid-column: 1 / -1;
-    }
 
 </style>
