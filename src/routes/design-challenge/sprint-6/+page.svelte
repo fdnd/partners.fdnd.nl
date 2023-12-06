754d67f4f96a2b0bac15bea1d1d43e3b86c92201
joostf <j.faber@hva.nl>
2023-12-06 22:31:50 +0100

    fixed li bullets, thanks @krijnhoetmer

--- src/routes/design-challenge/sprint-6/+page.svelte
+++ src/routes/design-challenge/sprint-6/+page.svelte
@@ -2,56 +2,60 @@
     import { Block, CollapsingColumns } from '@fdnd/components'
 </script>
 
-    <Block class="blue-on-green rounded" tag="section">
-        <h2>Design Challenge sprint 6: The Startup</h2>
+    <Block tag="section">
+        <Block class="blue-on-green rounded" tag="div">
+            <h2>Design Challenge sprint 6: The Startup</h2>
+            <p>Startdatum: <time>08-01-2024</time></p>
+            
+            <p>Eerstejaars studenten Frontend Design & Development (<a href="https://programma.fdnd.nl/">FDND</a>) gaan individueel of met een team een bedrijf helpen met een responsive website of online concept.</p>
+            
+            <p>In het eerste semester van de opleiding FDND hebben studenten veel geleerd over layout, responsive design, toegankelijkheid, interactie en het werken met een huisstijl. Alle studenten kunnen HTML, CSS en (een beetje) JavaScript.</p>
+            <p>Studenten gaan alles wat ze hebben geleerd toepassen om een opdracht voor een opdrachtgever te maken.</p>
+        </Block>
+
+        <Block class="green-on-blue rounded" tag="div">
+            <h3>Planning</h3>
+            <p>In 3 weken gaan studenten een opdracht realiseren voor een opdrachtgever, en we verwachten twee contactmomenten van opdrachtgevers:</p>
+            <ul>
+                <li><time>Maandag 8 januari 2024</time> krijgen de studenten een briefing bij de opdrachtgever. Tijdens de briefing maken de studenten kennis met het bedrijf en wordt besproken wat ze gaan maken.</li>
+                <li><time>Donderdag 25 januari</time> wordt de opdracht bij de opdrachtgever gepresenteerd en opgeleverd.</li>
+            </ul>
+            <p><time>Vrijdag 26 januari</time> vindt bij FDND in Amsterdam een expo plaats, waar alle studenten (zo'n 70 in totaal) hun werk laten zien. Opdrachtgevers zijn hier vanzelfsprekend ook welkom om rond te kijken en te borrelen.</p>
+            <p>Eventuele extra tussentijdse afspraken of check-ins kunnen natuurlijk met studenten gemaakt worden.</p>
+        </Block>
         
-        <p>In sprint 6: The Startup gaan eerstejaars studenten Frontend Design & Development (<a href="https://programma.fdnd.nl/">FDND</a>) individueel of met een team een bedrijf helpen met een responsive website of online concept.</p>
-        <p>Startdatum: <time>08-01-2024</time></p>
-        <p>In het eerste semester van de opleiding FDND hebben studenten veel geleerd over layout, responsive design, toegankelijkheid, interactie en het werken met een huisstijl. Alle studenten kunnen HTML, CSS en (een beetje) JavaScript.</p>
-        <p>In deze sprint gaan studenten alles wat ze hebben geleerd toepassen om een opdracht voor een opdrachtgever te maken.</p>
-    </Block> 
+        <Block class="green-on-blue rounded" tag="div">
+            <h3>Werkwijze</h3>
+            <ul>
+                <li>Op basis van een bestaand idee, concept of ontwerp gaan studenten FDND een responsive website of webpagina ontwerpen, maken en testen.</li>
+                <li>Studenten werken individueel of in een team aan een project.</li>
+                <li>Voor het project is er bestaande content zoals teksten, beeldmateriaal, video's, huisstijl of conceptmateriaal beschikbaar.</li>
+                <li>De code voor het project wordt publiekelijk op GitHub bewaard en gepubliceerd.</li>
+            </ul>
+        </Block>
 
-    <Block class="green-on-blue rounded" tag="section">
-        <h3>Planning</h3>
-        <p>In 3 weken gaan studenten een opdracht realiseren voor een opdrachtgever, en we verwachten twee contactmomenten van opdrachtgevers:</p>
-        <ul>
-            <li><time>Maandag 8 januari 2024</time> krijgen de studenten een briefing bij de opdrachtgever. Tijdens de briefing maken de studenten kennis met het bedrijf en wordt besproken wat ze gaan maken.</li>
-            <li><time>Donderdag 25 januari</time> wordt de opdracht bij de opdrachtgever gepresenteerd en opgeleverd.</li>
-        </ul>
-        <p><time>Vrijdag 26 januari</time> vindt bij FDND in Amsterdam een expo plaats, waar alle studenten (zo'n 70 in totaal) hun werk laten zien. Opdrachtgevers zijn hier vanzelfsprekend ook welkom om rond te kijken en te borrelen.</p>
-        <p>Eventuele extra tussentijdse afspraken of check-ins kunnen natuurlijk met studenten gemaakt worden.</p>
-    </Block>
-    
-    <Block class="green-on-blue rounded" tag="section">
-        <h3>Werkwijze</h3>
-        <ul>
-            <li>Op basis van een bestaand idee, concept of ontwerp gaan studenten FDND een responsive website of webpagina ontwerpen, maken en testen.</li>
-            <li>Studenten werken individueel of in een team aan een project.</li>
-            <li>Voor het project is er bestaande content zoals teksten, beeldmateriaal, video's, huisstijl of conceptmateriaal beschikbaar.</li>
-            <li>De code voor het project wordt publiekelijk op GitHub bewaard en gepubliceerd.</li>
-        </ul>
-    </Block>
+        <figure>
+            <img src="/img/review-sprint-12.jpeg" alt="" class="rounded">
+        </figure>
+        
+        <Block class="green-on-blue rounded" tag="div">
+            <h3>Ideeën en voorbeelden van projecten</h3>
+            <ul>
+                <li>Teampagina voor een corporate website met foto's, namen, teksten en …</li>
+                <li>Projectpagina voor een nieuw product met uitleg, beeldmateriaal, video, gebruikersreacties en …</li>
+                <li>Contactpagina met informatie, routebeschrijving, een contactformulier en …</li>
+                <li>Een nieuwe overzichtspagina met recente artikelen uit jullie blog …</li>
+                <li>Een interactief prototype van een idee of concept om in de de browser te testen …</li>
+                <li>Zelf ideeën? Laten we sparren en een leuke opdracht verzinnen voor onze studenten!</li>
+            </ul>
 
-    <figure>
-        <img src="/img/review-sprint-12.jpeg" alt="" class="rounded">
-    </figure>
-    
-    <Block class="green-on-blue rounded" tag="section">
-        <h3>Ideeën en voorbeelden van projecten</h3>
-        <ul>
-            <li>Teampagina voor een corporate website met foto's, namen, teksten en …</li>
-            <li>Projectpagina voor een nieuw product met uitleg, beeldmateriaal, video, gebruikersreacties en …</li>
-            <li>Contactpagina met informatie, routebeschrijving, een contactformulier en …</li>
-            <li>Een nieuwe overzichtspagina met recente artikelen uit jullie blog …</li>
-            <li>Een interactief prototype van een idee of concept om in de de browser te testen …</li>
-            <li>Zelf ideeën? Laten we sparren en een leuke opdracht verzinnen voor onze studenten!</li>
-        </ul>
+            <a href="https://programma.fdnd.nl/static-web/the-startup">Lees meer over het programma van sprint 6</a>  
+        </Block>
 
-        <a href="https://programma.fdnd.nl/static-web/the-startup">Lees meer over het programma van sprint 6</a>  
     </Block>
 
 <style>
-    :global(section), figure {
+    :global(div), figure {
         margin-bottom: 2rem;
         max-width:40em;
     }
@@ -61,7 +65,11 @@
     time {
         font-weight: bold;
     }
+    ul {
+
+    }
     li {
-        display:block
+        display:list-item;
+        margin-left:2rem;
     }
 </style>
