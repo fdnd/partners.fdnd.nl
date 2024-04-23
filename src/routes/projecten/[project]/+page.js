aaa55191bf4f96e72d61b97330f6cdacb007eb2f
joostf <j.faber@hva.nl>
2024-04-23 11:02:04 +0200

    :coffin: removed redundant slug folder

--- src/routes/projecten/[project]/+page.js
+++
@@ -1,9 +0,0 @@
-import { projectBySlug } from '$lib/graphql/projects';
-import { gql } from '$lib/graphql';
-
-export const load = async ({ params, fetch }) => {
-	const req = await fetch(import.meta.env.VITE_HYGRAPH_URL, { ...gql(projectBySlug, { slug: params.project }) });
-	const data = await req.json();
-
-	return { project: { ...data.data.projects[0] } };
-};
