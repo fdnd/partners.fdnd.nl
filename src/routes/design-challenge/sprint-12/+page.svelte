e6ae3c7e6cc19c14f1c04f5f171cc81e20d1130f
joostf <j.faber@hva.nl>
2024-05-13 12:08:11 +0200

    Update +page.svelte

--- src/routes/design-challenge/sprint-12/+page.svelte
+++ src/routes/design-challenge/sprint-12/+page.svelte
@@ -11,13 +11,13 @@
         // calling `cancel()` will prevent the submission
         // `submitter` is the `HTMLElement` that caused the form to be submitted
 
-        loading = true;
+        loading = true
 
         return async ({ result, update }) => {
             await setTimeout(() => {
                 loading = false
 
-                console.log('het is gelukt')
+                console.log(result)
             }, 2000);
             
             // `result` is an `ActionResult` object
@@ -94,7 +94,7 @@
 
         <!-- Aanmeldformulier -->
         <Block class="green-on-blue rounded" tag="div"></Block>
-            <form action="/design-challenge/sprint-12" method="POST" >
+            <form action="/design-challenge/sprint-12" method="POST" use:enhance={handleForm}>
 
                 {#if loading }
                     <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><style>.spinner_qM83{animation:spinner_8HQG 1.05s infinite}.spinner_oXPr{animation-delay:.1s}.spinner_ZTLf{animation-delay:.2s}@keyframes spinner_8HQG{0%,57.14%{animation-timing-function:cubic-bezier(0.33,.66,.66,1);transform:translate(0)}28.57%{animation-timing-function:cubic-bezier(0.33,0,.66,.33);transform:translateY(-6px)}100%{transform:translate(0)}}</style><circle class="spinner_qM83" cx="4" cy="12" r="3"/><circle class="spinner_qM83 spinner_oXPr" cx="12" cy="12" r="3"/><circle class="spinner_qM83 spinner_ZTLf" cx="20" cy="12" r="3"/></svg>
@@ -119,8 +119,6 @@
                 <button>Aanmelden</button>
             </form>
         </Block>
-    </div>
-</Block>
 
 <style>
     :global(div.global-hack div),
