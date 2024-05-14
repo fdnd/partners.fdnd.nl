01c0459e1823ae779dff02100d899d9708c3b2e6
joostf <j.faber@hva.nl>
2024-05-14 21:43:04 +0200

    :sparkles: added cta feature to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,49 @@
 </script>
 
 <PageHeader img='header-image.jpeg' />
+
+
+<section class="cta-row">
+    <div class="inner">
+        <header>
+            <h2 class="large-heading">Samenwerken @FDND</h2>
+			<p>Samenwerken met instellingen, bedrijven en het werkveld is één van de peilers van het onderwijsmodel van FDND, Hill (High Impact Learning that Lasts), anders gezegd het studio model. Studenten werken doorlopend aan echte projecten voor echte opdrachtgevers, krijgen les van experts uit het vakgebied en lopen stage bij bedrijven uit de creatieve industrie.</p>
+        </header>
+        <div class="cta">
+            <h3 class="small-heading">Projecten</h3>
+            
+            <p>Studenten werken individueel of in teams aan grote, langlopende projecten. Hierbij volgen ze verschillende aspecten van de scrum project methode, zoals sprint planning, stand-ups, sprint reviews en retrospect.</p>
+
+            <a href="/projecten" class="button is-dark"><span class="small-body">Meer over projecten</span></a>
+        </div>
+
+        <div class="cta">
+            <h3 class="small-heading">Design challenges</h3>
+            <p>De laatste sprints van semester 1 en semester 2 werken de studenten aan een design challenge voor een creative agency. Studenten krijgen een kijkje in de keuken van een creative agency en de agency maakt kennis met studenten van FDND.</p>
+
+            <a href="/Design challenges" class="button is-dark"><span class="small-body">Meer over design challenges</span></a>
+        </div>
+
+        <div class="cta">
+            <h3 class="small-heading">Stage</h3>
+            <p>In het laatste semester van de opleiding kunnen de studenten ervoor kiezen om stage te lopen. In een periode van 10 of 20 weken verzamelen studenten, tijdens de stage, bewijslast voor het eindassessment.</p>
+
+            <a href="/design-challenge" class="button is-dark "><span class="small-body">Meer over stage</span></a>
+        </div>
+
+		<div class="cta">
+            <h3 class="small-heading">Co-teachers</h3>
+            <p>Een groep studenten, wij noemen dat een squad (naar het Spotify organisatiemodel) wordt altijd begeleid door een docent/coach én een co-teacher. Met vaste en wisselende co-teacher zorgen we ervoor dat er altijd actuele kennis wordt overgedragen aan studenten.</p>
+
+            <a href="/co-teachers" class="button is-dark"><span class="small-body">Meer over co-teachers</span></a>
+        </div>
+
+		<div class="cta">
+            <h3 class="small-heading">Talks</h3>
+            <p>In elke sprint van de opleiding nodigen we een interessante spreker met relevante vakkennis uit, zodat de studenten (en de coaches) op de hoogte blijven van actuele ontwikkelingen in het vakgebied.</p>
+
+            <a href="/talks" class="button is-dark"><span class="small-body">Meer over talks</span></a>
+        </div>
+    </div>
+</section>
+
